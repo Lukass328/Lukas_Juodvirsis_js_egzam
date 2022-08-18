e8e8f5ea05ff894144456224a9120dd5f79bd984
Lukas <v123narco@gmail.com>
2022-08-18 08:53:51 +0300

    Task 1 completed

--- Task 1/script.js
+++ Task 1/script.js
@@ -8,3 +8,37 @@ pamatyti jo pateikto svorio kovertavimą į:
 Pastaba: atvaizdavimas turi būti matomas pateikus formą ir pateikiamas
 <div id="output"></div> viduje, bei turi turėti bent minimalų stilių;
 ------------------------------------------------------------------- */
+
+const formEl = document.forms[0];
+const searchEl = document.getElementById('search');
+const calcBtn = document.getElementById('submit-btn');
+const outputEl = document.getElementById('output');
+
+
+
+formEl.addEventListener('submit', (e) => {
+    e.preventDefault();
+   
+    const searchElValue = searchEl.value;
+    const atsWeight = {
+        pounds: searchElValue * 2.2046,
+        grams: searchElValue / 0.0010000,
+        ounces: searchElValue * 35.274,
+    }
+    
+    searchEl.value = '';
+
+    weightsTohtml(atsWeight);
+})
+
+
+function weightsTohtml (num) {
+  const divEl = document.createElement('div');
+  divEl.className = 'weights-container';
+  divEl.innerHTML = `<p> Pounds : ${num.pounds}</p>
+  <p> Grams : ${num.grams}</p>
+  <p> Ounces : ${num.ounces}</p>`;
+  outputEl.append(divEl);
+  
+}
+
