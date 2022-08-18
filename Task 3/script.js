0ba97918a6ee8a08836ec9840615dba472dcb19a
Lukas <v123narco@gmail.com>
2022-08-18 11:54:05 +0300

    Task 3 completed

--- Task 3/script.js
+++ Task 3/script.js
@@ -12,3 +12,36 @@ turėti bent minimalų stilių ir būti responsive;
 -------------------------------------------------------------------------- */
 
 const ENDPOINT = 'https://api.github.com/users';
+
+const showUsersBtn = document.getElementById('btn');
+const outputDivEl = document.getElementById('output');
+const messageEl = document.getElementById('message');
+
+ async function getUserFromApi() {
+    try {
+        const resp = await fetch(ENDPOINT);
+        if (!resp.ok)
+        throw new Error(`Failed ${resp.status}`);
+        const dataInJs = await resp.json();
+        showUsersBtn.addEventListener('click', () => {
+         makeCards(dataInJs);
+         messageEl.style.display = 'none'
+         
+    } );
+
+    } catch (error) {
+        console.warn(error.message);
+    }
+ }
+ getUserFromApi();
+
+ function makeCards(userArr) {
+    userArr.forEach((userObj) => {
+        const divEl = document.createElement('div');
+        divEl.className = 'users-cards';
+        divEl.innerHTML = `<h3 class="user-name" >${userObj.login}</h3>
+        <img class="user-pictures" src="${userObj.avatar_url}" alt="users avatar">`;
+        outputDivEl.append(divEl);
+    })
+ }
+
